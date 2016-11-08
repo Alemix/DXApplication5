54e6ac36540f1bf284f1543d43280a5e497e101c
Alemix0 <Alemix0@MAGW1010>
2016-11-08 21:37:27 +0300

    111

---
+++ DXApplication5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+
+
+namespace DXApplication5
+{
+    public partial class Form1 : RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitSkinGallery();
+        
+            sqlDataSource1.Fill();
+        }
+        void InitSkinGallery()
+        {
+            SkinHelper.InitSkinGallery(rgbiSkins, true);
+        }
+        BindingList<Person> gridDataList = new BindingList<Person>();
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "lS_DBF_Для_ПереносаDataSet.spragrt". При необходимости она может быть перемещена или удалена.
+           this.spragrtTableAdapter.Fill(this.lS_DBF_Для_ПереносаDataSet.spragrt);
+
+        }
+    }
+}
